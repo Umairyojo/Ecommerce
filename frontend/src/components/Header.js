ceb4512c2ea4182a02478789101796487c93825c
Umairyojo <umairahmed89586z@gmail.com>
2025-01-05 10:33:45 +0530

    json configs

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -53,11 +53,11 @@ const Header = () => {
     }
   }
   return (
-    <header className='h-16 shadow-md bg-white fixed w-full z-40'>
+    <header className='h-20 shadow-md bg-white fixed w-full z-40'>
       <div className=' h-full container mx-auto flex items-center px-4 justify-between'>
-            <div className=''>
+            <div className='flex flex-row'>
                 <Link to={"/"}>
-                    <Logo w={90} h={50}/>
+                    <Logo w={10} h={10}/>
                 </Link>
             </div>
 
